2203f0134d9b9d6a7a984f4d15780decdb4a482c
Ryan Fiorini <rfiorini@bankmobile.com>
2021-04-26 14:32:59 -0400

    Add project files.

---
+++ BitwiseOperatorsExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BitwiseOperatorsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            int x = 5, y = 10, result;
+            result = x & y;
+            Console.WriteLine("Bitwise AND: " + result);
+            result = x | y;
+            Console.WriteLine("Bitwise OR: " + result);
+            result = x ^ y;
+            Console.WriteLine("Bitwise XOR: " + result);
+            result = ~x;
+            Console.WriteLine("Bitwise Complement: " + result);
+            result = x << 2;
+            Console.WriteLine("Bitwise Left Shift: " + result);
+            result = x >> 2;
+            Console.WriteLine("Bitwise Right Shift: " + result);
+
+            Console.WriteLine("End");
+
+        }
+    }
+}
